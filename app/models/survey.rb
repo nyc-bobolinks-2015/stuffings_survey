99390b6c0f3a39de6490fd844551126e3eed05ef
Arcangelo Petretta & Michael <pair+arcpetretta.heylittlehouse@devbootcamp.com>
2015-11-21 15:18:26 -0500

    modified survey controller/model to reflect the creation of an array that holds questions and statistics.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,6 +4,8 @@ class Survey < ActiveRecord::Base
   has_many :answers, through: :choices, dependent: :destroy
   belongs_to :user
 
+  attr_accessor :stats_for_all_answers
+
   def initialize
     super
     @stats_for_all_answers = []
@@ -18,11 +20,15 @@ class Survey < ActiveRecord::Base
     (number_of_same_answers / number_of_participants.to_f * 100).round(2)
   end
 
-  def stats_for_all_answers=(value)
-    @stats_for_all_answers << value
-  end
+  # def stats_for_all_answers=(value)
+  #   @stats_for_all_answers << value
+  # end
 
-  def stats_for_all_answers
-    @stats_for_all_answers
+  # def stats_for_all_answers
+  #   @stats_for_all_answers
+  # end
+
+  def clear_stats
+    stats_for_all_answers = []
   end
 end
