a7e23468b885e2fcc59da69dd0c852506db9dc1f
Arcangelo Petretta & Michael <pair+arcpetretta.heylittlehouse@devbootcamp.com>
2015-11-21 14:46:25 -0500

    Add statistics for answers in the Survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,10 +1,15 @@
 class Survey < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   has_many :choices, through: :questions, dependent: :destroy
-  has_many :answers, dependent: :destroy
+  has_many :answers, through: :choices, dependent: :destroy
   belongs_to :user
 
   def number_of_participants
-    @number_of_participants = Answers.find_by(survey: self.id).users.all.count
+    self.answers.map{|answer| answer.user_id}.uniq.count
+  end
+
+  def percent_answered_same(question_id, choice_id)
+    number_of_same_answers = self.answers.where(question_id: question_id, choice_id: choice_id, survey_id: self.id).count
+    (number_of_same_answers / number_of_participants.to_f * 100).round(2)
   end
 end
