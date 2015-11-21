5a6d329b50eabc7fdedfcb4be73e857afb02706d
Arcangelo Petretta & Michael <pair+arcpetretta.heylittlehouse@devbootcamp.com>
2015-11-21 16:09:51 -0500

    create a sessions controllers and moved routes pertaining to sessions into session controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,35 +6,35 @@ get '/users' do
   erb :'/users/index'
 end
 
-post '/users/login' do
-  @user = User.find_by(email: params[:email])
-  if @user && @user.password == params[:password]
-    session[:user_id] = @user.id
-    redirect ('/surveys')
-  else
-    # include error message that user information isn't correct
-    erb :'/users/index'
-  end
-end
+# post '/users/login' do
+#   @user = User.find_by(email: params[:email])
+#   if @user && @user.password == params[:password]
+#     session[:user_id] = @user.id
+#     redirect ('/surveys')
+#   else
+#     # include error message that user information isn't correct
+#     erb :'/users/index'
+#   end
+# end
 
 get '/users/new' do
   erb :'/users/new'
 end
 
-post '/users/new' do
-  @user = User.new(params[:user])
-  if @user.save
-    session[:user_id] = @user.id
-    redirect ("/surveys")
-  else
-    erb :'/users/new'
-  end
-end
+# post '/users' do
+#   @user = User.new(params[:user])
+#   if @user.save
+#     session[:user_id] = @user.id
+#     redirect ("/surveys")
+#   else
+#     erb :'/users/new'
+#   end
+# end
 
-get '/users/logout' do
-  session.clear
-  redirect("/")
-end
+# get '/users/logout' do
+#   session.clear
+#   redirect("/")
+# end
 
 get '/users/:id' do
   @user = User.find(params[:id])
