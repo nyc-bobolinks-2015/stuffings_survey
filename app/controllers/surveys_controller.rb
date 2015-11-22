32f50d95c8ceef535a0c878acfec1646f2c257a8
Courtney & Michael <pair+aquarian5.heylittlehouse@devbootcamp.com>
2015-11-21 19:00:45 -0500

    Fix controller to display next question

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -51,16 +51,17 @@ get '/surveys/:survey_id/questions/:question_id' do
 end
 
 post '/surveys/:survey_id/questions/:question_id' do
-  answer = Answer.new(survey: params[:survey_id], question: params[:question_id], choice: params[:choice_id], user: session[:user_id])
+  choice = Choice.find_by(text: params[:choice])
+  answer = Answer.new(survey_id: params[:survey_id], question_id: params[:question_id], choice: choice, user_id: session[:user_id])
   survey = Survey.find(params[:survey_id])
   if answer.question == survey.questions.count && answer.save #checks if answer is last answer in survey
     stat_for_answer = survey.percent_answered_same(params[:question_id], answer.choice_id)
-    survey.stats_for_all_answers << stat_for_answer
+    survey.stats_for_all_answers=(stat_for_answer)
     redirect('/surveys/:survey_id/statistics')
   elsif answer.save
     stat_for_answer = survey.percent_answered_same(params[:question_id], answer.choice_id)
-    survey.stats_for_all_answers << stat_for_answer
-    redirect("/surveys/#{answer.survey}/questions/#{answer.question + 1}")
+    survey.stats_for_all_answers=(stat_for_answer)
+    redirect("/surveys/#{survey.id}/questions/#{params[:question_id].to_i + 1}")
   else
     @error = "Please select a valid answer"
     erb :'surveys/show'
